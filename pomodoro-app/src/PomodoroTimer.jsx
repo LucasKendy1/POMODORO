32478c0906cb2d9753150b94c159686678f6b977
LucasKendy1 <87815996+LucasKendy1@users.noreply.github.com>
2024-09-10 20:34:46 -0300

    som

--- pomodoro-app/src/PomodoroTimer.jsx
+++ pomodoro-app/src/PomodoroTimer.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "primereact/button"; // Importando o Button do PrimeReact
-import "primeicons/primeicons.css"; // Importando PrimeIcons
-import styles from "./pomodorotimer.module.css"; // Importando o CSS Module
+import { Button } from "primereact/button";
+import "primeicons/primeicons.css";
+import styles from "./pomodorotimer.module.css";
 
 const PomodoroTimer = () => {
   const [minutes, setMinutes] = useState(25);
@@ -9,6 +9,9 @@ const PomodoroTimer = () => {
   const [isActive, setIsActive] = useState(false);
   const [pomodoros, setPomodoros] = useState(0);
   const [isBreak, setIsBreak] = useState(false);
+
+  // Crie uma instância de Audio fora do useEffect para evitar criar múltiplas instâncias
+  const alarm = new Audio("/time-over.wav");
 
   useEffect(() => {
     let timer;
@@ -23,6 +26,7 @@ const PomodoroTimer = () => {
       }, 1000);
     } else if (isActive && minutes === 0 && seconds === 0) {
       setIsActive(false);
+      alarm.play(); // Reproduzir o som quando o timer termina
       if (!isBreak) {
         setPomodoros((prevPomodoros) => prevPomodoros + 1);
         setIsBreak(true);
@@ -52,28 +56,35 @@ const PomodoroTimer = () => {
   };
 
   const handleShortBreak = () => {
-    setIsActive(false); // Pausa o timer antes de ajustar
+    setIsActive(false);
     setMinutes(5);
     setSeconds(0);
     setIsBreak(true);
   };
 
   const handleLongBreak = () => {
-    setIsActive(false); // Pausa o timer antes de ajustar
+    setIsActive(false);
     setMinutes(15);
     setSeconds(0);
     setIsBreak(true);
   };
 
   const handlePomodoro = () => {
-    setIsActive(false); // Pausa o timer antes de ajustar
+    setIsActive(false);
     setMinutes(25);
     setSeconds(0);
     setIsBreak(false);
   };
 
+//   //testar o som
+//   const handleSound = () => {
+//     alarm.play();
+//   }
+
+
   return (
     <div className={styles.container}>
+        
       <div className={styles.topButtons}>
         <Button
           label="Pomodoro"
